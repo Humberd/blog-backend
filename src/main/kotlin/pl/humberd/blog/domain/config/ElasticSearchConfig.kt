1ca7e9f247dc25213526c8939515c7adb1a708b1
humberd <humberd2@gmail.com>
2021-01-03 20:13:43 +0100

    Added elasticsearch url from properties and from env

--- src/main/kotlin/pl/humberd/blog/domain/config/ElasticSearchConfig.kt
+++ src/main/kotlin/pl/humberd/blog/domain/config/ElasticSearchConfig.kt
@@ -1,6 +1,7 @@
 package pl.humberd.blog.domain.config
 
 import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
@@ -8,12 +9,14 @@ import org.springframework.data.elasticsearch.client.RestClients
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
 
 @Configuration
-class ElasticSearchConfig : AbstractElasticsearchConfiguration() {
+class ElasticSearchConfig(
+    @Value("\${elasticsearch.url}") private val elasticsearchUrl: String
+) : AbstractElasticsearchConfiguration() {
 
     @Bean
     override fun elasticsearchClient(): RestHighLevelClient {
         return ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(elasticsearchUrl)
             .build()
             .let {
                 RestClients.create(it).rest()
